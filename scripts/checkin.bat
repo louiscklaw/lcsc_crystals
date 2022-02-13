305ec3451f1eba13539898c1a4a0650b9fe1a323
louiscklaw <logickee@gmail.com>
2022-02-13 14:19:58 +0800

    update lcsc_crystals,

---
+++ scripts/checkin.bat
@@ -0,0 +1,12 @@
+git add . 
+git commit -m"update lcsc_crystals,"
+
+git pull
+
+git push
+
+cd ..
+  git pull origin master
+  git add lcsc_crystals
+  git commit -m"update lcsc_crystals,"
+cd lcsc_crystals
